98ca1735416788d593acb3eac39ecdf4a5d0d720
mariaclaraseixa <maria.clara.seixa@gmail.com>
2023-03-26 10:57:49 -0300

    Programa solicitado pela professor na aula de programacao

---
+++ inversor.c
@@ -0,0 +1,32 @@
+//
+// Created by maria clara seixa scheffel on 26/03/2023.
+//
+#include <stdio.h>
+int main() {
+    int number,
+        hundred,
+        dozen,
+        unit,
+        inverse,
+        min,
+        max;
+
+    min =  100;
+    max = 1000;
+    number =  983;
+    if(number > min && number < max) {
+        dozen = number % 100;
+        hundred = (number - dozen) / 100;
+        unit = dozen % 10;
+        dozen = (dozen - unit) / 10;
+        inverse = (unit * 100) + (dozen * 10) + hundred;
+        printf("number  = %i\n", number);
+        printf("hundred = %i\n", hundred);
+        printf("dozen  = %i\n", dozen);
+        printf("unit = %i\n", unit);
+        printf("inverse = %i\n", inverse);
+    } else {
+        printf("The number is %i it must be bigger than %i  and smaller than %i", number, min, max);
+    }
+    return 0;
+}
